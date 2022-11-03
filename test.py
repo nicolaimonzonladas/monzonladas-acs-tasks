28d67569f0666dd1fa7fb4417221a5dbb670bbde
Nicolai ML <47255212+nicolaimonzonladas@users.noreply.github.com>
2022-11-03 21:29:51 +0000

    Changes

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("hello sir")
+list1 = [2,5,15,36,47]
+list2 = [18,39,42]
+
+sorted_list =[]
+
+print(list1)
+print(list2)
+
+while list1 and list2:
+    if list1[0] <  list2[0]:
+        sorted_list.append(list1.pop(0))
+    else:
+        sorted_list.append(list2.pop(0))
+
+sorted_list += list1
+sorted_list += list2
+
+print(sorted_list)
+
+# print(sorted(list1 + list2))
