73f0a467c91ec770bec334a4a279a5aa45a3a835
Nicolai ML <47255212+nicolaimonzonladas@users.noreply.github.com>
2022-11-07 14:14:47 +0000

    Create 2.pong.py

---
+++ pyGame/2.pong.py
@@ -0,0 +1,75 @@
+import pygame
+
+# -- Global Constants
+
+ballWidth = 20
+xVal = 50
+yVal = 200
+
+xDir = 1
+yDir = 4
+
+screenW = 500
+screenH = 500
+
+# -- Colours
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (50,50,255)
+YELLOW = (255,255,0)
+
+# -- Initialise PyGame
+pygame.init()
+
+# -- Blank Screen
+size = (screenW,screenH)
+screen = pygame.display.set_mode(size)
+
+# -- Title of new window/screen
+pygame.display.set_caption("Pong")
+
+# -- Exit game flag set to false
+done = False
+
+# -- Manages how fast screen refreshes
+clock = pygame.time.Clock()
+
+
+### -- Game Loop
+
+
+while not done:
+    # -- User input and controls
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        #End If
+    #Next event
+    
+    # -- Game logic goes after this comment
+    
+    if yVal < 0:
+        yDir = yDir * -1
+    
+    if yVal > screenH-ballWidth:
+        yDir = yDir * -1
+    
+    xVal = xVal + xDir
+    yVal = yVal + yDir
+    
+    print(yVal, yDir)
+        
+    # -- Screen background is BLACK
+    screen.fill (BLACK)
+    
+    # -- Draw here
+    pygame.draw.rect(screen, BLUE, (xVal,yVal,ballWidth,ballWidth))
+
+    # -- flip display to reveal new position of objects
+    pygame.display.flip()
+    
+    # - The clock ticks over
+    clock.tick(60)
+    
+    #End While - End of game loop
+pygame.quit()
