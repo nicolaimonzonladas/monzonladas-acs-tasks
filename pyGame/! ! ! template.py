d6151cf609f6c35bd36d56d29dab895abf18cfa9
Nicolai ML <47255212+nicolaimonzonladas@users.noreply.github.com>
2022-11-21 11:56:26 +0000

    Updated template file

--- pyGame/! ! ! template.py
+++ pyGame/! ! ! template.py
@@ -1,55 +1,55 @@
 import pygame
 
-# -- Global Constants
+# Global Constants
 
-# -- Colours
+# Colours
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 BLUE = (50,50,255)
 YELLOW = (255,255,0)
 
-# -- Initialise PyGame
+# Initialise PyGame
 pygame.init()
 
-# -- Blank Screen
+# Blank Screen
 size = (640,480)
 screen = pygame.display.set_mode(size)
 
-# -- Title of new window/screen
+# Title of new window/screen
 pygame.display.set_caption("My Window")
 
-# -- Exit game flag set to false
+# Exit game flag set to false
 done = False
 
-# -- Manages how fast screen refreshes
+# Manages how fast screen refreshes
 clock = pygame.time.Clock()
 
 
-### -- Game Loop
+### Game Loop
 
 
 while not done:
-    # -- User input and controls
+    # User input and controls
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        #End If
-    #Next event
+        # End If
+    # Next event
     
-    # -- Game logic goes after this comment
+    # Game logic goes after this comment
     
-    # -- Screen background is BLACK
+    # Screen background is BLACK
     screen.fill (BLACK)
     
-    # -- Draw here
+    # Draw here
     pygame.draw.rect(screen, BLUE, (220,165,200,150))
     pygame.draw.circle(screen, YELLOW, (40,100),40,0)
     
-    # -- flip display to reveal new position of objects
+    # Flip display to reveal new position of objects
     pygame.display.flip()
     
-    # - The clock ticks over
+    # The clock ticks over
     clock.tick(60)
     
-    #End While - End of game loop
+    # End While End of game loop
 pygame.quit()
