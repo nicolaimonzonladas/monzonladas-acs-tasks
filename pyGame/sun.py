405c0b813c0897ebea52c46cc81be1725e8e8371
Nicolai ML <47255212+nicolaimonzonladas@users.noreply.github.com>
2022-11-28 10:39:02 +0000

    Perfected sun.py

--- pyGame/sun.py
+++ pyGame/sun.py
@@ -4,28 +4,30 @@ import pygame
 
 # -- Colours
 
+WHITE = (255,255,255)
 BLACK = (0,0,0)
 ORANGE = (255,150,0)
 BLUE = (50,50,255)
+GREEN = (50,155,0)
 # YELLOW = (y,y,0)
 
 # -- Initialise PyGame
 pygame.init()
 
 # -- Blank Screen
-size = (500,500)
+size = (640,240)
 screen = pygame.display.set_mode(size)
 
 # -- Title of new window/screen
-pygame.display.set_caption("My Window")
+pygame.display.set_caption("Sun")
 
 # -- Exit game flag set to false
 done = False
 
-circX = 360
-circY = 230
+circX = 0
+circY = 0
 
-y = 255
+y = 0
 
 step = 2
 stepY = 2
@@ -48,38 +50,46 @@ while not done:
     # -- Game logic goes after this comment
     
     # -- Screen background is BLACK
-    screen.fill (BLACK)
+    screen.fill(BLACK)
     
     # -- Draw here
     YELLOW = (y,y,0)
-    pygame.draw.rect(screen, YELLOW, (0,0,500,500))
-    pygame.draw.circle(screen, ORANGE, (circX,circY),20,0)
-    
+    pygame.draw.rect(screen, YELLOW, (0,0,700,500))
+    pygame.draw.circle(screen, WHITE, (circX,int(((3/1280)*(circX - 320)**2 ))),20,0)
+    pygame.draw.circle(screen, GREEN, (320, 300), 100, 0)    
+    pygame.draw.rect(screen, BLUE, (300, 180, 40, 40))
    
-    circX += step
-    if circX > 402 or circX < 80 :
-        step = -step 
+    circX += 1
+
+    if circX > 640:
+        circX = 0
+   
+    # circX += step
+    # if circX > 402 or circX < 80 :
+    #     step = -step 
         
-    circY += stepY
-    if circY > 402 or circY < 80 :
-        stepY = -stepY
+    # circY += stepY
+    # if circY > 402 or circY < 80 :
+    #     stepY = -stepY
     
-    if circY > 250:
-        if y > 5:
-            y -= 5
-    elif circY < 250:
-        if y < 250:
-            y += 5
+    # if circY > 250:
+    #     if y > 5:
+    #         y -= 5
+    # elif circY < 250:
+    #     if y < 250:
+    #         y += 5
     
     # -- flip display to reveal new position of objects
     pygame.display.flip()
     
     # - The clock ticks over
-    clock.tick(60)
+    clock.tick(120)
     
-    if y > 250:
-        y = 250
-    elif y < 5:
+    if circX > 0 and circX < 320:
+        y += 0.7
+    elif circX > 320:
+        y -= 0.7
+    elif circX < 5:
         y = 0
     
     #End While - End of game loop
