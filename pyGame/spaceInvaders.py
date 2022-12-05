af1421b7c96585fa3328ea108f2dcab87cc6aed9
Nicolai ML <47255212+nicolaimonzonladas@users.noreply.github.com>
2022-12-05 12:26:17 +0000

    IN PROGRESS
    Sir - in progress making the player class

--- pyGame/spaceInvaders.py
+++ pyGame/spaceInvaders.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 # Global Constants
 
@@ -12,14 +14,72 @@ YELLOW = (255,255,0)
 pygame.init()
 
 # Blank Screen
-size = (640,480)
+size = (1000,800)
 screen = pygame.display.set_mode(size)
 
 # Title of new window/screen
-pygame.display.set_caption("My Window")
+pygame.display.set_caption("Invader")
+
+# Define the class Invader which is a sprite
+class Invader(pygame.sprite.Sprite):
+    # Define the constructor for Invader
+    def __init__(self, color, width, height):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 1000)
+        self.rect.y = random.randrange(-50,0)
+
+# Define the class Player which is a sprite
+class Player(pygame.sprite.Sprite):
+    # Define the constructor for Player
+    def __init__(self, color, width, height, speed):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 20
+        # Set speed of the sprite
+        self.speed = 0
+    # Class update function - runs for each pass through the game loop
+    def update(self):
+        self.rect.y = self.rect.y + self.speed
+        # Respawn code
+        if self.rect.y > 800:
+            # print("landed")
+            self.rect.y = -5
+            self.rect.x = random.randrange(0,995)
+        
+        
+
 
 # Exit game flag set to false
 done = False
+
+# Create a list of the Invader blocks
+Invader_group = pygame.sprite.Group()
+# Create a list of all sprites
+all_sprites_group = pygame.sprite.Group()
+
+
+# Create the Invaders
+number_of_flakes = 50 
+for x in range(number_of_flakes):
+    my_Invader = Invader(WHITE, 5, 5)
+    Invader_group.add(my_Invader)
+    all_sprites_group.add(my_Invader)
+    
+# Create the Player
+player = Player(YELLOW, 20, 20, 1)
+all_sprites_group.add(player)
 
 # Manages how fast screen refreshes
 clock = pygame.time.Clock()
@@ -39,11 +99,13 @@ while not done:
     # Game logic goes after this comment
     
     # Screen background is BLACK
-    screen.fill (BLACK)
+    screen.fill(BLACK)
     
     # Draw here
-    pygame.draw.rect(screen, BLUE, (220,165,200,150))
-    pygame.draw.circle(screen, YELLOW, (40,100),40,0)
+    
+    all_sprites_group.draw(screen)
+    # pygame.draw.rect(screen, BLUE, (220,165,200,150))
+    # pygame.draw.circle(screen, YELLOW, (40,100),40,0)
     
     # Flip display to reveal new position of objects
     pygame.display.flip()
